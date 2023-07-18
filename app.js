b636272b9d9850d645c6ce94e5c1db4c570c5509
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2023-07-18 22:13:21 +0700

    update-res-final

--- app.js
+++ app.js
@@ -1,46 +1,62 @@
 
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('.main-content');
+// const sections = document.querySelectorAll('.section');
+// const sectBtns = document.querySelectorAll('.controls');
+// const sectBtn = document.querySelectorAll('.control');
+// const allSections = document.querySelector('.main-content');
 
-function PageTransitions() {
-    //  Nút: click active class
-    for (let i = 0; i < sectBtn.length; i++) {
-        sectBtn[i].addEventListener('click', function() {
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
+// function PageTransitions() {
+//     //  Nút: click active class
+//     for (let i = 0; i < sectBtn.length; i++) {
+//         sectBtn[i].addEventListener('click', function() {
+//             let currentBtn = document.querySelectorAll('.active-btn');
+//             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+//             this.className += ' active-btn';
+//         })
+//     }
+
+//     // Section: Active class
+//     allSections.addEventListener('click', (e) => {
+//         const id= e.target.dataset.id;
+//         if (id) {
+//             // Remove button selected from the other btns
+//             sectBtns.forEach((btn) => {
+//                 btn.classList.remove('active')
+//             })
+//             e.target.classList.add('active')
+
+//             // Hide other sections
+//             // Ẩn các phần khác
+//             sections.forEach((section) => {
+//                 section.classList.remove('active')
+//             })
+
+//             const element = document.getElementById(id);
+//             element.classList.add('active');
+//         }
+//     })
+
+//     // Toggle theme
+//     const themeBtn = document.querySelector('.theme-btn');
+//     themeBtn.addEventListener('click', () => {
+//         let element = document.body;
+//         element.classList.toggle('light-mode')
+//     })
+// }
+
+// PageTransitions();
+
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
         })
-    }
+    });
 
-    // Section: Active class
-    allSections.addEventListener('click', (e) => {
-        const id= e.target.dataset.id;
-        if (id) {
-            // Remove button selected from the other btns
-            sectBtns.forEach((btn) => {
-                btn.classList.remove('active')
-            })
-            e.target.classList.add('active')
-
-            // Hide other sections
-            // Ẩn các phần khác
-            sections.forEach((section) => {
-                section.classList.remove('active')
-            })
-
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
     })
+})();
 
-    // Toggle theme
-    const themeBtn = document.querySelector('.theme-btn');
-    themeBtn.addEventListener('click', () => {
-        let element = document.body;
-        element.classList.toggle('light-mode')
-    })
-}
-
-PageTransitions();
